4a74da6fd9105dc3985b15137ba2e21f8f6094a5
Jochen Dueckminor <jochen@dueckminor.de>
2021-04-05 14:06:07 +0000

    updates

--- scripts/lib/fritzbox.sh
+++ scripts/lib/fritzbox.sh
@@ -12,9 +12,9 @@ FritzBox::GetExternalIP() {
     curl "http://fritz.box:49000/igdupnp/control/WANIPConn1" \
         -H "Content-Type: text/xml; charset=\"utf-8\"" \
         -H "SoapAction:urn:schemas-upnp-org:service:WANIPConnection:1#GetExternalIPAddress" \
-        -d "<?xml version='1.0' encoding='utf-8'?> 
-                <s:Envelope s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/' 
-                    xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'> 
+        -d "<?xml version='1.0' encoding='utf-8'?>
+                <s:Envelope s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'
+                    xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'>
                   <s:Body>
                     <u:GetExternalIPAddress xmlns:u='urn:schemas-upnp-org:service:WANIPConnection:1' /> 
                   </s:Body>

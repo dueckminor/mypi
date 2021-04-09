aecaf3e3a07c835557fd611c101d426f404ff679
Jochen Dueckminor <jochen@dueckminor.de>
2021-04-09 16:22:14 +0000

    added missing files

---
+++ lib/python/mypi/config/config.py
@@ -0,0 +1,28 @@
+import yaml
+import os
+
+rootDir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))))
+mypiYml = None
+
+def GetConfig():
+    global rootDir
+    global mypiYml
+
+    with open(rootDir+"/config/mypi.yml") as stream:
+        try:
+            mypiYml = yaml.safe_load(stream)
+        except:
+            return None
+    return mypiYml['config']
+
+def WriteIfChanged(fileName, content):
+    if os.path.exists(fileName):
+        with open(fileName,mode='r') as file:
+            existingContent = file.read()
+            if existingContent == content:
+                return
+    with open(fileName, mode='w') as file:
+        file.write(content)
+
+def WriteYamlEtc(name, content):
+    WriteIfChanged(rootDir+"/etc/"+name,yaml.dump(content))
